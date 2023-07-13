cd6e089561e9a9cc5b080ce1ca7b9d84abd66003
Belt0M <belongtomars@gmail.com>
2023-07-13 21:54:45 +0200

    Completed Order page

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import {
+	ColumnDirective,
+	ColumnsDirective,
+	ContextMenu,
+	Edit,
+	ExcelExport,
+	Filter,
+	GridComponent,
+	Inject,
+	Page,
+	PdfExport,
+	Resize,
+	Sort,
+} from '@syncfusion/ej2-react-grids'
+import { Header } from '../components'
+import { ordersData, ordersGrid } from '../data/dummy.js'
 
 const Orders = () => {
-	return <div>Orders</div>
+	return (
+		<div className='m-6 mt-12 md:m-10 p-6 md:p-10 bg-white rounded-xl'>
+			<Header category='Page' title='Orders' />
+			<GridComponent
+				id='gridcomp'
+				dataSource={ordersData}
+				allowPaging
+				allowSorting
+			>
+				<ColumnsDirective>
+					{ordersGrid.map((item, index) => (
+						<ColumnDirective key={index} {...item} />
+					))}
+				</ColumnsDirective>
+				<Inject
+					services={[
+						Resize,
+						ExcelExport,
+						Filter,
+						Page,
+						Sort,
+						ContextMenu,
+						Edit,
+						PdfExport,
+					]}
+				/>
+			</GridComponent>
+		</div>
+	)
 }
 
 export default Orders
