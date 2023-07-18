43fee3bb9803fcad1d1fa978c2162018d766e52a
Belt0M <belongtomars@gmail.com>
2023-07-18 23:49:07 +0200

    ColorPicker app completed

--- src/pages/ColorPicker.tsx
+++ src/pages/ColorPicker.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import {
+	ColorPickerComponent,
+	ColorPickerEventArgs,
+} from '@syncfusion/ej2-react-inputs'
+import { Header } from '../components'
 
 const ColorPicker = () => {
-	return <div>ColorPicker</div>
+	const onChange = (args: ColorPickerEventArgs) => {
+		const preview = document.getElementById('preview')
+		if (preview) {
+			preview.style.backgroundColor = args.currentValue.hex
+		}
+	}
+
+	return (
+		<div className='m-4 mt-24 md:m-10 p-2 md:p-10 bg-white rounded-xl drop-shadow-md'>
+			<Header category='App' title='Color Picker' />
+			<div className='text-center'>
+				<div id='preview' />
+				<div className='flex items-center justify-center gap-20 flex-wrap'>
+					<div>
+						<p className='mb-4 mt-2 text-2xl font-semibold'>Inline Palette</p>
+						<ColorPickerComponent
+							id='inline-palette'
+							mode='Palette'
+							modeSwitcher={false}
+							inline
+							showButtons={false}
+							change={onChange}
+						/>
+					</div>
+					<div>
+						<p className='mb-4 mt-2 text-2xl font-semibold'>Inline Picker</p>
+						<ColorPickerComponent
+							id='inline-palette'
+							mode='Picker'
+							modeSwitcher={false}
+							inline
+							showButtons={false}
+							change={onChange}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ColorPicker
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import {
+	HtmlEditor,
+	Inject,
+	RichTextEditorComponent,
+	Toolbar,
+} from '@syncfusion/ej2-react-richtexteditor'
+import { Header } from '../components'
 
 const Editor = () => {
-	return <div>Editor</div>
+	const toolbarSettings: object = {
+		items: [
+			'Bold',
+			'Italic',
+			'Underline',
+			'StrikeThrough',
+			'FontName',
+			'FontSize',
+			'FontColor',
+			'BackgroundColor',
+			'LowerCase',
+			'UpperCase',
+			'|',
+			'Formats',
+			'Alignments',
+			'Outdent',
+			'Indent',
+			'|',
+			'Image',
+			'|',
+			'Print',
+			'SourceCode',
+			'FullScreen',
+			'|',
+			'Undo',
+			'Redo',
+		],
+	}
+	return (
+		<div className='m-4 mt-24 md:m-10 p-2 md:p-10 bg-white rounded-xl drop-shadow-md'>
+			<Header category='App' title='Editor' />
+			<RichTextEditorComponent toolbarSettings={toolbarSettings}>
+				<Inject services={[Toolbar, HtmlEditor]} />
+			</RichTextEditorComponent>
+		</div>
+	)
 }
 
 export default Editor
